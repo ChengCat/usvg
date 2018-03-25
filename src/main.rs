7a86baf76726d32da891c07abe1b2bb44b9d9ee1
Reizner Evgeniy <razrfalcon@gmail.com>
2018-03-25 22:45:48 +0300

    Logging enabled.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #[macro_use] extern crate clap;
 extern crate usvg;
 extern crate svgdom;
+extern crate fern;
+extern crate log;
 
 
 use std::io::Write;
 use std::fs::File;
+use std::fmt;
 
 use clap::{ App, Arg };
 
@@ -44,6 +47,13 @@ fn main() {
         dpi: value_t!(args.value_of("dpi"), u32).unwrap() as f64,
         keep_named_groups: args.is_present("keep-named-groups"),
     };
+
+    fern::Dispatch::new()
+        .format(log_format)
+        .level(log::LevelFilter::Warn)
+        .chain(std::io::stderr())
+        .apply().unwrap();
+
     let tree = usvg::parse_tree_from_file(in_svg, &re_opt).unwrap();
 
     let dom_opt = svgdom::WriteOptions {
@@ -82,4 +92,26 @@ fn is_dpi(val: String) -> Result<(), String> {
     } else {
         Err(String::from("Invalid DPI value."))
     }
+}
+
+fn log_format(
+    out: fern::FormatCallback,
+    message: &fmt::Arguments,
+    record: &log::Record,
+) {
+    let lvl = match record.level() {
+        log::Level::Error => "Error",
+        log::Level::Warn  => "Warning",
+        log::Level::Info  => "Info",
+        log::Level::Debug => "Debug",
+        log::Level::Trace => "Trace",
+    };
+
+    out.finish(format_args!(
+        "{} (in {}:{}): {}",
+        lvl,
+        record.target(),
+        record.line().unwrap_or(0),
+        message
+    ))
 }
